301d9b63c756da82ec133d04c882eed36fb67926
leronewilliams <dayman26@aol.com>
2015-03-06 15:37:14 -0600

    Joining data update
    Almost done with the Rmd.

---
+++ 02 Data Wrangling/Joining Data.R
@@ -0,0 +1,52 @@
+# Query to build the join data frame
+require("RCurl")
+require("jsonlite")
+require("dplyr")
+require("tidyr")
+require("ggplot2")
+
+
+Above4cali <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="SELECT * FROM ABOVE4CALI"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_law229', PASS='orcl_law229', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+
+Above4OU <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="SELECT * FROM ABOVE4OU"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_law229', PASS='orcl_law229', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+
+Recentca <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="SELECT * FROM RECENTCA"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_law229', PASS='orcl_law229', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+
+Recentou <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="SELECT * FROM RECENTOU"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_law229', PASS='orcl_law229', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+
+names(Above4OU);names(Above4cali);names(Recentca);names(Recentou)
+
+left_join(Above4cali, Above4OU, by = "MAG") %>% ggplot(aes(x=LATITUDE.x, y=LONGITUDE.x, color=PLACE.x))+geom_point()
+
+right_join(Above4cali, Above4OU, by = "DEPTH") %>% ggplot(aes(x=LATITUDE.x, y=LONGITUDE.x, color=PLACE.x))+geom_point()
+
+inner_join(Above4cali, Above4OU, by = "MAG") %>% ggplot(aes(x=LATITUDE.x, y=LONGITUDE.x, color=PLACE.x))+geom_point()
+
+full_join(Above4cali, Above4OU, by = "DEPTH") %>% ggplot(aes(x=LATITUDE.x, y=LONGITUDE.x, color=PLACE.x))+geom_point()
+
+semi_join(Above4cali, Above4OU, by = "MAG") %>% ggplot(aes(x=LATITUDE, y=LONGITUDE, color=PLACE))+geom_point()
+
+anti_join(Above4cali, Above4OU, by = "DEPTH") %>% ggplot(aes(x=LATITUDE, y=LONGITUDE, color=PLACE))+geom_point()
+
+bind_rows(Above4cali, Above4OU) %>% ggplot(aes(x=LATITUDE, y=LONGITUDE, color=PLACE))+geom_point()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
